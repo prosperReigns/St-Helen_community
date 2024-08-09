48a9429db8d622f8060749cff658b10d95a5535d
prosperReigns <prosperkinging7@gmail.com>
2024-08-09 10:10:43 +0100

    create core app

--- st_helen/core/models.py
+++ st_helen/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank_profile_image.png')
+    location = models.CharField(max_length=100, blank=True)
