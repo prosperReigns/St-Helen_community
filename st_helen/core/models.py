d57ca12e039504e6b26c752edea5593080455ed4
tenia619 <teniolaadebisi3@gmail.com>
2024-08-15 10:44:28 +0100

    changes made

--- st_helen/core/models.py
+++ st_helen/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 # Create your models here.
 
@@ -9,3 +11,11 @@ class Profile(models.Model):
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank_profile_image.png')
     location = models.CharField(max_length=100, blank=True)
+
+class Posts(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="post_images")
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
--- st_helen/core/views.py
+++ st_helen/core/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile #imports Profile from the current directory
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="login") #redirects to login page if someone tries to access the home page
 def index(request):
     return render(request, 'index.html')
 
@@ -44,27 +46,52 @@ def signup(request):
         return render(request, 'signup.html')
 
 def login(request):
-    if request.method = "POST":
+    if request.method == "POST": #POST methods send data from the client to the server 
+            username = request.POST["username"] #passing in the argument entered from the form
+            password = request.POST["password"]
 
-        if password == password2: #checking if passwords are equal (validation)
-            if User.objects.filter(email=email).exists() and User.objects.filter(username=username).exists(): 
-               user_login = auth.authenticate(username=username, password=password)
-               auth.login(request, user_login)
-
-            elif username == "": #checks if username is empty
-                messages.info(request, "Username cannot be empty")
-                return redirect("signup")
+            user_login = auth.authenticate(username=username, password=password)
+            
+            if user_login is not None: #checks if the user login exists
+                auth.login(request, user_login)
+                return redirect("/")
             else:
-                return redirect("login") #edit later 
-
-        else:
-            messages.info(request, "Password does not match") #*
-            return redirect("login")
+                messages.info(request, "Invalid credentials") #user doesn't exist message 
+                return redirect("signup")
 
     else:
         return render(request, 'login.html')
 
-
-
+@login_required(login_url="login") #only accessible if you're logged in
 def logout(request):
+    auth.logout(request)
     return render(request, 'logout.html')
+
+def settings(request):
+    try:
+        user_profile = Profile.objects.get(user=request.user) #getting the information for the currently logged in user
+    except Profile.DoesNotExist:
+        user_profile = Profile.objects.create(user=request.user)
+
+    if request.method == "POST":
+        if request.FILES.get("image") == None: #checking if user uploaded an image to see if we use the default or theirs
+            bio = request.POST["bio"]
+            location = request.POST["location"]
+            image = user_profile.profileimg
+
+            user_profile.profileimg = image #updating the profile models with the required values 
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get("image") != None:
+            bio = request.POST["bio"]
+            location = request.POST["location"]
+            image = request.FILES.get("image")
+
+            user_profile.profileimg = image #updating the profile models with the required values 
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect ("settings")
+
+    return render(request, "settings.html", {"user_profile": user_profile}) #passing user profile as an object to the html (frontend)
